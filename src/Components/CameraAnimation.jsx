3ba4e42cf397baee18c096e029ecba4b57081c92
BomberQLF <tmurphyparis@gmail.com>
2025-02-22 18:24:23 +0100

    Reflect Added + Image

--- src/Components/CameraAnimation.jsx
+++ src/Components/CameraAnimation.jsx
@@ -2,11 +2,11 @@ import { useThree } from "@react-three/fiber";
 import gsap from "gsap";
 
 
-const CameraAnimation = ({ hovered }) => {
+const CameraAnimation = ({ clicked }) => {
   const { camera } = useThree();
 
   gsap.to(camera.position, {
-    z: hovered ? 2.5 : 5,
+    z: clicked ? 3 : 5,
     duration: 2,
     ease: "power2.inOut",
   });
--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -3,16 +3,24 @@ import { OrbitControls } from "@react-three/drei";
 import { useState } from "react";
 import CameraAnimation from "../Components/CameraAnimation";
 import Frame from "../Components/Frame";
-
+import { Fog } from "three";
 
 const Gallery = () => {
-  const [hovered, setHovered] = useState(false);
+  const [clicked, setClicked] = useState(false);
 
   return (
-    <Canvas style={{ background: "#050505" }} camera={{ position: [0, 0, 5], fov: 75 }}>
-      <CameraAnimation hovered={hovered} />
-      <Frame onClick={() => setHovered(!hovered)} /> {/* Plane avec clic */}
+    <Canvas
+      style={{ background: "#222" }}
+      camera={{ position: [0, -0.5, 5], fov: 75 }}
+      onCreated={({ scene }) => {
+        scene.fog = new Fog(0x333333, 2, 10); 
+      }}
+    >
+      <CameraAnimation clicked={clicked} />
+      <Frame onClick={() => setClicked(!clicked)} url="/public/assets/green-img.jpg" />
+
       <OrbitControls />
+      <ambientLight intensity={0.5} />
     </Canvas>
   );
 };
