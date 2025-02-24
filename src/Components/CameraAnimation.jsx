ff40c5663b3fdef59e0af6488ebef9f56938b446
BomberQLF <tmurphyparis@gmail.com>
2025-02-24 14:40:40 +0100

    Poitionnement frames bon

--- src/Components/CameraAnimation.jsx
+++ src/Components/CameraAnimation.jsx
@@ -3,25 +3,51 @@ import { useThree } from "@react-three/fiber";
 import gsap from "gsap";
 
 const CameraAnimation = ({ clickedFrame, initialCameraPosition }) => {
-  // Utilisation de usethtee ici pour pouvoir accéder à la caméra de threejs et pour éviter qu'elle croit que c'est une simple variable
   const { camera } = useThree();
 
-
-  // AJOUTER LE PROPS ROTATION VENANT DE LA FRAME --> pour que dans le composant camera je puisse séléctionner la rotation de la camera en fonction de la frame
   useEffect(() => {
-    if (clickedFrame) {
+    if (Array.isArray(clickedFrame)) {
       gsap.to(camera.position, {
         x: clickedFrame[0],
         y: clickedFrame[1],
-        z: clickedFrame[2] + 3,
+        z: clickedFrame[2] + 3, 
+        duration: 2,
+        ease: "power2.inOut",
+      });
+      gsap.to(camera.rotation, {
+        x: 0,
+        y: 0,
+        z: 0,
+        duration: 2,
+        ease: "power2.inOut",
+      });
+    } else if (clickedFrame && clickedFrame.position && clickedFrame.rotation) {
+      gsap.to(camera.position, {
+        x: clickedFrame.position[0],
+        y: clickedFrame.position[1], 
+        z: clickedFrame.position[2] + 3,
+        duration: 2,
+        ease: "power2.inOut",
+      });
+      gsap.to(camera.rotation, {
+        x: 0,
+        y: clickedFrame.rotation[1],
+        z: 0,
         duration: 2,
         ease: "power2.inOut",
       });
     } else {
       gsap.to(camera.position, {
         x: initialCameraPosition[0],
-        y: initialCameraPosition[1],
+        y: initialCameraPosition[1], // Ajuster la hauteur de la caméra
         z: initialCameraPosition[2],
+        duration: 2,
+        ease: "power2.inOut",
+      });
+      gsap.to(camera.rotation, {
+        x: 0,
+        y: 0,
+        z: 0,
         duration: 2,
         ease: "power2.inOut",
       });
