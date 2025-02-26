51501c592cf565f907462cf482d2889c0b3b7985
BomberQLF <tmurphyparis@gmail.com>
2025-02-27 00:46:38 +0100

    Correction Mineure

--- src/Components/Billeterie/BilletTarif.jsx
+++ src/Components/Billeterie/BilletTarif.jsx
@@ -1,9 +1,9 @@
 const BilletTarif = ({ ticketName, price, counter, increment, decrement }) => {
   return (
     <div>
-      <div className="p-4 flex flex-col gap-2 lg:gap-12 w-fit border border-white border-solid rounded-[22px] mb-4 md:flex-row md:gap-36 lg:gap-48">
+      <div className="p-4 flex flex-col gap-2 lg:gap-12 w-fit border border-white border-solid rounded-[22px] mb-4 md:flex-row md:gap-36 lg:gap-48 lg:mb-16">
         <div className="w-[13rem]">
-          <p className="text-white text-sm md:text-xl">{ticketName}</p>
+          <p className="text-white text-sm md:text-xl lg:w-[14rem]">{ticketName}</p>
           <p className="text-white text-sm md:text-xl">{price}</p>
         </div>
         <div className="flex items-center gap-4">
