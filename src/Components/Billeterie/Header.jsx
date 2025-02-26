888174ad0d7067769db01ea0dda4598a879f7ca1
BomberQLF <tmurphyparis@gmail.com>
2025-02-26 17:09:15 +0100

    Correction taille titre

--- src/Components/Billeterie/Header.jsx
+++ src/Components/Billeterie/Header.jsx
@@ -1,7 +1,7 @@
 const Header = ({ title, subtitle }) => {
     return (
         <header>
-            <h1 className="uppercase text-white text-2xl md:text-4xl lg:text-6xl">{title}</h1>
+            <h1 className="uppercase text-white text-2xl md:text-4xl lg:text-5xl">{title}</h1>
             <p className="text-white text-xs mt-4 md:text-xl md:mt-8 lg:mt-12 lg:text-xl">{subtitle}</p>
         </header>
     );
