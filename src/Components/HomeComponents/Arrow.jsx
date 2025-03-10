0a64f12db57d4442848174ff1f60caa19dc0bdb3
BomberQLF <tmurphyparis@gmail.com>
2025-03-10 09:53:16 +0100

    Responsive Accueil

--- src/Components/HomeComponents/Arrow.jsx
+++ src/Components/HomeComponents/Arrow.jsx
@@ -7,11 +7,11 @@ const ArrowComp = () => {
   return (
     <>
       <img
-        className="absolute right-[35px] bottom-[2rem] h-[200px] cursor-pointer"
+        className="absolute right-[35px] bottom-[2rem] h-[200px] cursor-pointer md:h-[260px] lg:h-[320px] lg:right-[42px]"
         src={Arrow}
         alt={Arrow}
       />
-      <span className="absolute uppercase right-[-27px] bottom-[8.8rem] text-white arrow_accueil text-xs cursor-pointer">
+      <span className="absolute uppercase right-[-27px] bottom-[8.8rem] text-white arrow_accueil text-xs cursor-pointer md:text-lg md:right-[-3.5rem] md:bottom-[10.8rem] lg:text-2xl lg:right-[-5rem] lg:bottom-[13rem]">
         {t("arrow_text")}
       </span>
     </>
--- src/Components/HomeComponents/Header.jsx
+++ src/Components/HomeComponents/Header.jsx
@@ -1,28 +1,28 @@
 import { useTranslation } from "react-i18next";
 
 const Header = () => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
   return (
     <div>
       <div className="p-12">
-        <h1 className="uppercase text-white font-bold text-4xl">Marie Curie</h1>
-        <h2 className="text-right font-thin text-white text-lg">
+        <h1 className="uppercase text-white font-bold text-4xl md:text-6xl md:ml-14 lg:ml-32 xl:text-8xl xl:text-center xl:m-0 xl:tracking-wide"><span>Marie</span> Curie</h1>
+        <h2 className="text-right font-thin text-white text-lg md:text-2xl md:mr-16 lg:mr-32 lg:text-3xl xl:mr-52 xl:text-4xl">
           {t("titre_accueil")}
         </h2>
         <div className="mt-6">
-          <div className="border border-white rounded-[22px] px-6 py-2 text-center relative">
-            <div>
-              <a className="text-white font-thin text-sm" href="#">
-                {t("trailer_accueil")}
-              </a>
+          <div>
+            <div className="border border-white rounded-[22px] px-6 py-2 text-center md:m-auto md:max-w-[350px] lg:max-w-[430px]">
+              <div>
+                <a className="text-white font-thin text-sm md:text-lg lg:text-xl" href="#">
+                  {t("trailer_accueil")}
+                </a>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div>
-        EMPTY
-      </div>
+      <div>EMPTY</div>
     </div>
   );
 };
